4f691ca3f519e9451c76849d9b0e19843e3b0445
nikita <erohinnikita100@gmail.com>
2022-07-04 15:39:59 +0300

    valid_device_done

--- client/src/components/modals/DeviceModal/DeviceModal.tsx
+++ client/src/components/modals/DeviceModal/DeviceModal.tsx
@@ -1,4 +1,4 @@
-import { FC, SetStateAction, useState, MouseEvent, ChangeEvent, useEffect } from "react"
+import { FC, SetStateAction, useState, MouseEvent, ChangeEvent, useEffect, useLayoutEffect } from "react"
 import {  IDeviceInfo, ITypeAndBrand } from "../../../interface/interface"
 import style from './DeviceModal.module.css'
 import { v4 as uuidv4 } from 'uuid';
@@ -7,6 +7,7 @@ import { adminFormValidation } from "../../../validation/DeviceFormValidation";
 
 import ErrorModal from "../../ErrorModal";
 import { deviceInfoValidation } from "../../../validation/DeviceInfoValidation";
+import { resolve } from "node:path/win32";
 
 const DeviceModal
 :FC<{types:ITypeAndBrand[] | undefined, brands: ITypeAndBrand[] | undefined}> = 
@@ -24,6 +25,7 @@ const DeviceModal
     const [info, setInfo] = useState<IDeviceInfo[]>([])
     const [infoError, setInfoError] = useState<any>('')
     const [deviceFormError, setDeviceFormError] = useState<string | ''>('')
+    const [clickCount, setClickCount] = useState<any>(1)
 
     const [createDevice, {data, isLoading,isSuccess}] = useCreateDeviceMutation()
 
@@ -41,9 +43,10 @@ const DeviceModal
             id: uuidv4()}])
     }
 
-    const changeInfo = (key:string, value:string, id:string):void => {
+    const changeInfo = (key:string, keyValid:string, value:string, id:string):void => {
         setInfoError([])
-        setInfo(info.map(i => i.id === id ? {...i, [key]: value}: i))
+        setInfo(info.map(i => i.id === id ? {...i, [key]: value, 
+            [keyValid]: deviceInfoValidation(value)}: i))
     }
 
     const removeInfo = (id:string) => {
@@ -52,6 +55,7 @@ const DeviceModal
 
     const addDevice = (e:MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
+        setClickCount(clickCount + 1)
         // const typeIdValid = adminFormValidation(typeId, setTypeIdError)
         // const brandIdValid = adminFormValidation(brandId, setBrandIdError)
         // const nameValid = adminFormValidation(name, setNameError)
@@ -62,39 +66,77 @@ const DeviceModal
                 descriptionValid:deviceInfoValidation(i.description)
             }))
         )
-
+        // function infoResult () {
+        //     const test = info.find(i => i.titleValid === "clear")
+        //     return new Promise((res, rej) => res(test))
+        // }
         
-        setInfoError(info.filter( i => i.titleValid === "clear"))
+        // const result = await infoResult()
+        
+        // console.log(result)
 
-        // i.titleValid === "error" || 
-        // i.descriptionValid=== "error" || 
-        // i.titleValid==="firstTime" || 
-        // i.descriptionValid==="firsTime")
 
+        // for(let i = 0; i < info.length; i++) {
+        //     if(deviceInfoValidation(info[i].title) !== "clear"){
+        //         info[i].titleValid = 'error'
+        //         valid = false
+        //     } else {
+        //         info[i].titleValid = 'clear'
+        //         valid = true
+        //     }
+        // }
+
+
+        // if(valid) {
+        //     sendForm()
+        //     setDeviceFormError('')
+        // } else {
+        //     setDeviceFormError('исправьте форму перед сохранением устройства')
+        // }
+        sendForm()
     }
 
+    useEffect(()=> {
+        console.log('infoValidation work!' + infoError)
+
+        // setInfo(info.map(i => 
+        //     ({...i, titleValid:deviceInfoValidation(i.title),
+        //         descriptionValid:deviceInfoValidation(i.description)
+        //     }))
+        // )
+    },[infoError])
+
+    useEffect(()=> {  
+        // console.log('infoError work')
+        info.map(i => {
+            if(i.titleValid !== "clear" || i.descriptionValid !== "clear") {
+                setInfoError(true)
+            } else {
+                setInfoError(false)
+            }
+        })
+    },[info])
+
+
     const sendForm = () => {
-        const formData = new FormData()
-        formData.append('typeId', String(typeId))
-        formData.append('brandId', String(brandId))
-        formData.append('name', name)
-        formData.append('price', String(price))
-        formData.append('img', image)
-        formData.append('info', JSON.stringify(info))
-        createDevice(formData)
+        if(!infoError) {
+            console.log("SENDED!")
+        } else {
+            console.log("DONT SEND")
+            setDeviceFormError("исправьте форму!")
+        }
+        // const formData = new FormData()
+        // formData.append('typeId', String(typeId))
+        // formData.append('brandId', String(brandId))
+        // formData.append('name', name)
+        // formData.append('price', String(price))
+        // formData.append('img', image)
+        // formData.append('info', JSON.stringify(info))
+        // createDevice(formData)
     }
 
     // typeIdValid && brandIdValid && nameValid && 
     //         // image && priceValid
-
-    useEffect(()=> {
-        console.log(infoError)
-        if(!infoError) {
-            sendForm()
-        } else {
-            setDeviceFormError('исправьте форму перед сохранением устройства')
-        }
-    },[infoError])
 
     if(isLoading) return <h3>saved...</h3>
     if(isSuccess) console.log('save complete')
@@ -156,13 +198,13 @@ const DeviceModal
                     <div key={i.id}>
                         <input 
                             value={i.title}
-                            onChange={e => changeInfo('title', e.target.value, i.id)}
+                            onChange={e => changeInfo('title', 'titleValid',e.target.value, i.id)}
                             placeholder="введите название"
                             style={{'background': ((i.titleValid==="clear") || (i.titleValid==="firstTime")) ? "white" : "red"}}
                         />
                         <input 
                             value={i.description}
-                            onChange={e => changeInfo('description', e.target.value, i.id)}
+                            onChange={e => changeInfo('description', 'descriptionValid',e.target.value, i.id)}
                             placeholder="введите описание"
                             style={{'background':(( i.descriptionValid==="clear" )|| (i.descriptionValid==="firstTime" ))? "white" : "red"}}
                         />
