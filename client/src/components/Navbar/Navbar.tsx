4f1e6b80c80daa8efdc45563a19a1802b7729e5c
nikita <erohinnikita100@gmail.com>
2023-05-15 12:46:40 +0530

    done

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,118 +1,163 @@
-import { useContext, useState } from "react"
-import { NavLink } from "react-router-dom"
-import { PagesEnum } from "../../enums/enums"
-import { useCheckQuery, useRemoveCookieMutation } from "../../store/apiSlice/userSlice"
-import { LoginActions } from "../../App"
-import { initialState } from "../../store/reactReducer/authFormReducer"
-import { animated, useSpring } from '@react-spring/web'
-import NavbarBurger from "../../images/svg/Navbar_lines/NavbarBurger"
-import Locker from "../../images/svg/enter/Locker"
-import ShopIcon from "../../images/svg/shop/ShopIcon"
-import SuperAdminIcon from "../../images/svg/SuperAdminIcon"
-import BasketIcon from "../../images/svg/BasketIcon"
-import LockIcon from '@mui/icons-material/Lock';
-import AddBusinessOutlinedIcon from '@mui/icons-material/AddBusinessOutlined';
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import { Avatar, Paper } from "@mui/material"
-import Logo_2 from "../../images/svg/Logo_2"
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import Logo_2 from '../../images/svg/Logo_2';
 
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-const Navbar = () => {
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-    const [removeCookie] = useRemoveCookieMutation()
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const {data, isLoading} = useCheckQuery()
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const dispatch = useContext(LoginActions)
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    const [navbarVisible, setNavbarVisible] = useState<boolean>(true)
+  return (
+    <AppBar position="static" sx={{bgcolor: "#D3D3D3"}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          {/* <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography> */}
 
-    const logout = () => {
-        removeCookie()
-        dispatch!({type: 'reset', payload: initialState})
-    }
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            //   sx={{color: 'black', fontSize: 150}}
+            >
+              <MenuIcon sx={{color: 'black', fontSize: 35}} />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Logo_2 sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, fontSize: 75 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              color: '#000',
+              textDecoration: 'none',
+            }}
+          >
+            Art AI 
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
 
-    const navbarView = ()=> {
-        setNavbarVisible(!navbarVisible)
-    }
-
-    if(isLoading){
-        return <h3>loading navbar...</h3>
-    }
-
-    return (
-        <AppBar 
-            position="static"
-            sx={{bgcolor: '#D3D3D3'}}
-        >
-                <Logo_2 
-                    sx={{fontSize: 100}}
-                />
-                
-                {data?.role ?
-                    <NavLink
-                        className={ "navbar_element"}
-                        to={PagesEnum.ENTER}
-                        onClick={() => logout()}
-                    >   
-                        {/* <Locker /> */}
-                        <span className="navbarText">выйти</span>        
-                    </NavLink>
-                    :
-                    <NavLink
-                        className={ "navbar_element"}
-                        to={PagesEnum.ENTER}
-                        onClick={()=> navbarView()}
-                    >
-                        <span>войти</span>
-                        <LockIcon/>
-                    </NavLink>
-                
-                }
-                <NavLink
-                    className={  "navbar_element" }
-                    to={PagesEnum.SHOP}
-                >
-                    <span 
-                        onClick={()=> navbarView()}
-                        className="col s4"
-                    >
-                        магазин
-                    </span>
-                    <AddBusinessOutlinedIcon />
-                </NavLink>
-                {(data?.role ===  "ADMIN" ) && 
-                    <NavLink
-                        className={ "navbar_element" } 
-                        to={PagesEnum.ADMIN_PAGE}
-                    >
-                        {/* <SuperAdminIcon /> */}
-                        <span className="navbarText">админ панель</span>
-                    </NavLink>
-                }
-                {(data?.role === "SUPER_ADMIN") && 
-                    <NavLink
-                        className={ "navbar_element" } 
-                        to={PagesEnum.SUPER_ADMIN_PAGE}
-                    >
-                        {/* <SuperAdminIcon /> */}
-                        <span className="navbarText">super admin page</span>
-                    </NavLink>
-                }
-                {(data?.role === "USER") && 
-                    <NavLink
-                        className={ "navbar_element" } 
-                        to={PagesEnum.BASKET}
-                        onClick={()=> navbarView()}
-                    >
-                        {/* <BasketIcon /> */}
-                        <span className="navbarText">корзина</span>
-                    </NavLink>
-                }
-                <div>{data?.email}</div>
-           
-        </AppBar>
-    )
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
-
-export default Navbar
+export default ResponsiveAppBar;
