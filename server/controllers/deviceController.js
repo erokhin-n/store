3facb4ec8539800dd7b89799f23258b8603b61b4
nikita <erohinnikita100@gmail.com>
2023-08-18 11:53:46 +0530

    imgur

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -2,34 +2,70 @@ const {Device, DeviceInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
 const uuid = require('uuid')
 const path = require('path')
+const axios = require('axios');
+const fs = require('fs');
+
+const Device = require('./models/Device'); 
+
+async function uploadToImgur(imagePath, clientId) {
+	try {
+	  const image = fs.readFileSync(imagePath, { encoding: 'base64' });
+  
+	  const response = await axios.post('https://api.imgur.com/3/image', image, {
+		headers: {
+		  Authorization: `Client-ID ${clientId}`,
+		},
+	  });
+  
+	  const imageUrl = response.data.data.link;
+	  return imageUrl;
+	} catch (error) {
+	  console.error('Error uploading to Imgur:', error.response.data);
+	  throw new Error('Image upload to Imgur failed');
+	}
+}
 
 class DeviceController {
-	async create(req,res, next) {
-		try{
-			let {name, price, brandId, typeId, info} = req.body
-			const existName = await Device.findOne({where: {name}})
-			if(existName) throw ApiError.conflict('такое название устройства уже существует')
-			const {img} = req.files
-			let fileName = uuid.v4() + ".jpg"
-			img.mv(path.resolve(__dirname, '..', 'static', fileName))
-			const device = await Device.create({name, price, brandId, typeId, img: fileName})
-
-			if(info){
-				info = JSON.parse(info)
-				info.forEach(i => 
-					DeviceInfo.create({
-						title: i.title,
-						description: i.description,
-						deviceId: device.id
-					})
-				)
-			}
-
-			return res.json(device)
-		} catch(e) {
-			next(e)
+	async create(req, res, next) {
+		try {
+		  let { name, price, brandId, typeId, info } = req.body;
+		  const existName = await Device.findOne({ where: { name } });
+		  if (existName) throw ApiError.conflict('такое название устройства уже существует');
+		  const { img } = req.files;
+		  const fileName = uuid.v4() + '.jpg';
+	
+		  // Путь к временной директории, где сохраняются изображения
+		  const imagePath = path.resolve(__dirname, '..', 'static', fileName);
+	
+		  img.mv(imagePath);
+	
+		  // Используйте ваш Client ID для авторизации на Imgur
+		  const clientId = '69672255265524a';
+	
+		  // Загрузка изображения на Imgur
+		  const imageUrl = await uploadToImgur(imagePath, clientId);
+	
+		  const device = await Device.create({ name, price, brandId, typeId, img: imageUrl });
+	
+		  if (info) {
+			info = JSON.parse(info);
+			info.forEach((i) =>
+			  DeviceInfo.create({
+				title: i.title,
+				description: i.description,
+				deviceId: device.id,
+			  })
+			);
+		  }
+	
+		  // Удаление временного файла после загрузки на Imgur
+		  fs.unlinkSync(imagePath);
+	
+		  return res.json(device);
+		} catch (e) {
+		  next(e);
 		}
-	}
+	  }
 	async getAll(req,res) {
 		let {brandId, typeId, limit, page} = req.query
 		// page = page || 1
