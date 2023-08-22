256eaf341fa242ca4efb2514661f5b0a26d919cd
nikita <erohinnikita100@gmail.com>
2023-08-22 09:31:44 +0530

    y

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -40,7 +40,7 @@ export function App() {
                             <AppRouter />
                         </Grid>
                         <Grid item xs={12}>
-                            <Footer />
+                            {/* <Footer /> */}
                         </Grid>
                     </Grid>
                 </BrowserRouter>
