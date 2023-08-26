088c9d76ba0ec994f005b55d71ce15e4cccf80b3
nikita <erohinnikita100@gmail.com>
2023-08-26 23:52:11 +0530

    fix-6

--- client/src/components/DeviceItem.tsx
+++ client/src/components/DeviceItem.tsx
@@ -17,7 +17,8 @@ const DeviceItem: FC<IDeviceProps<IDevice>> = ({ device, basketId }) => {
     useEffect(() => {
         async function loadImageUrl() {
             try {
-                const storageRef = ref(storage, 'images/' + device.img.name);
+                console.log("Device image:", device.img);
+                const storageRef = ref(storage, 'images/' + device.img);
                 const url = await getDownloadURL(storageRef);
                 setImageUrl(url);
             } catch (error) {
