f1ddacefff1c86a268880c13a9b7982065fd80e6
nikita <erohinnikita100@gmail.com>
2023-08-28 15:23:59 +0530

    initial

--- client/src/components/DeviceItem.tsx
+++ client/src/components/DeviceItem.tsx
@@ -1,62 +1,51 @@
-import { FC, useEffect, useState } from "react";
-import { IDevice, IDeviceProps } from "../interface/interface";
-import { useAddDeviceMutation } from "../store/apiSlice/basketSlice";
-import { useNavigate } from "react-router-dom";
-import { PagesEnum } from "../enums/enums";
-import Card from "@mui/material/Card";
-import { Button, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { FC } from "react"
+import { IDevice, IDeviceProps } from "../interface/interface"
+import BasketButton from "../images/svg/BasketButton"
+import { useAddDeviceMutation, useGetBasketQuery } from "../store/apiSlice/basketSlice"
+import { useCheckQuery } from "../store/apiSlice/userSlice"
+import { useGetProductCardQuery } from "../store/apiSlice/deviceSlice"
+import { useNavigate } from "react-router-dom"
+import { PagesEnum } from "../enums/enums"
+import Card from "@mui/material/Card"
+import { Button, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { ref, getDownloadURL } from 'firebase/storage';
-import storage from '../firebaseConfig'; // Импортируйте storage из вашего файла
 
-const DeviceItem: FC<IDeviceProps<IDevice>> = ({ device, basketId }) => {
-    const navigate = useNavigate();
-    const [addDevice] = useAddDeviceMutation();
-    const [imageUrl, setImageUrl] =useState<string | null>(null);
 
-    useEffect(() => {
-        async function loadImageUrl() {
-            try {
-                if (device.img) {
-                    const storageRef = ref(storage, 'images/' + device.img);
-                    const url = await getDownloadURL(storageRef);
-                    setImageUrl(url);
-                }
-            } catch (error) {
-                console.error('Error loading image URL:', error);
-            }
-        }
-    
-        loadImageUrl();
-    }, [device.img]);
-    
-    const goToProductCard = () => {
-        navigate(PagesEnum.PRODUCT_CARD + '/' + device.id);
-    }
+const DeviceItem:FC<IDeviceProps<IDevice>> = ({device, basketId}) => {
+
+    const navigate = useNavigate()
+
+    const [addDevice, {data}] = useAddDeviceMutation()
 
     const saveDeviceInBasket = () => {
-        addDevice({ device, basketId });
+        addDevice({device, basketId}) 
+    }
+
+    const goToProductCard = () => {
+        navigate(PagesEnum.PRODUCT_CARD + '/' + device.id) 
     }
 
     return (
-        <Card
-            sx={{
+        <Card 
+            sx={{ 
                 maxWidth: 300,
                 background: '#A0A0A0'
             }}
+            
         >
-            <CardActionArea
-                onClick={goToProductCard}
+            <CardActionArea 
+                onClick={()=> goToProductCard()}
             >
-                <CardMedia
+                <CardMedia 
                     component="img"
-                    height='250'
-                    image={imageUrl || ''}
+                    height='250' 
+                    image={`http://localhost:5000/${device.img}`} 
                     alt="device"
                 />
                 <CardContent>
-                    <Typography
-                        gutterBottom variant="h5"
+                    <Typography 
+                        gutterBottom variant="h5" 
+                        // component="div"
                         color='#fff'
                     >
                         {device.name}
@@ -67,15 +56,15 @@ const DeviceItem: FC<IDeviceProps<IDevice>> = ({ device, basketId }) => {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button
+                <Button 
                     variant="contained"
-                    onClick={saveDeviceInBasket}
+                    onClick={()=> saveDeviceInBasket()}
                 >
-                    <ShoppingCartIcon />
+                    <ShoppingCartIcon/>
                 </Button>
             </CardActions>
         </Card>
     )
 }
 
-export default DeviceItem;
+export default DeviceItem
