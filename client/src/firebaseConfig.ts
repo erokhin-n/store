a207afff08ef1f5dee489a31da1c96a508e65995
nikita <erohinnikita100@gmail.com>
2023-08-28 16:57:06 +0530

    initial

--- client/src/firebaseConfig.ts
+++ client/src/firebaseConfig.ts
@@ -1,29 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyBYLLg1BO0hccqu1fLOGqqUjUHwjR2sCZE",
+  authDomain: "storepictures-db9c6.firebaseapp.com",
+  projectId: "storepictures-db9c6",
+  storageBucket: "storepictures-db9c6.appspot.com",
+  messagingSenderId: "152604144843",
+  appId: "1:152604144843:web:9a44fdd9327fbb9502174e",
+  measurementId: "G-XG4T2EPTQY"
+};
 
-    apiKey: "AIzaSyBYLLg1BO0hccqu1fLOGqqUjUHwjR2sCZE",
-  
-    authDomain: "storepictures-db9c6.firebaseapp.com",
-  
-    projectId: "storepictures-db9c6",
-  
-    storageBucket: "storepictures-db9c6.appspot.com",
-  
-    messagingSenderId: "152604144843",
-  
-    appId: "1:152604144843:web:9a44fdd9327fbb9502174e",
-  
-    measurementId: "G-XG4T2EPTQY"
-  
-  };
-  
-
-// Инициализируйте Firebase с использованием конфигурации
-const app = initializeApp(firebaseConfig);
-
-// Получите ссылку на Firebase Storage
-const storage = getStorage(app);
-
-export default storage;
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
