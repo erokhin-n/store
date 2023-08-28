6cf7d2188c4e526aed509654cbeab667688740c1
nikita <erohinnikita100@gmail.com>
2023-08-28 15:27:02 +0530

    initial

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -2,39 +2,41 @@
 import DeviceList from "../components/DeviceList"
 import Footer from "../components/Footer"
 import { useAppSelector } from "../hooks/useDispatchAndSelector"
-import { IDevice } from "../interface/interface"
 import { useGetAllDevicesQuery} from "../store/apiSlice/deviceSlice"
 import { useGetAllTypesQuery } from "../store/apiSlice/typeSlice"
 
 
 const Shop = () => {
-    const { data: devices, isLoading, isSuccess, isError, error } = useGetAllDevicesQuery();
 
-    let deviceList;
+    const {
+        data: devices,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetAllDevicesQuery()
 
-    if (isLoading) {
-        deviceList = "load...";
-    } else if (isSuccess) {
-        if (devices.rows) {
-            deviceList = devices.rows.length ? (
-                <DeviceList
-    devices={devices.rows.map((device: IDevice) => ({
-        ...device,
-        img: device.imageFileName // Передаем имя файла изображения
-    }))}
-/>
+    let deviceList
 
-            ) : (
-                <h3>устройств нет</h3>
-            );
+    if(isLoading) {
+        deviceList = "load..."
+    } else if(isSuccess){
+        if(devices.rows) {
+            deviceList = (devices.rows.length) ? 
+            <DeviceList devices={devices.rows} /> : 
+            <h3>устройств нет</h3>
         } else {
-            deviceList = <h3>no devices in mocks</h3>;
+            deviceList = <h3>no devices in mocks</h3>
         }
-    } else if (isError) {
-        deviceList = <div>error</div>;
+    } else if(isError) {
+        deviceList = <div>error</div>
     }
 
-    return <div className="shop">{deviceList}</div>;
-};
+    return (
+        <div className="shop">
+            {deviceList}
+        </div>
+    )
+}
 
-export default Shop;
+export default Shop
