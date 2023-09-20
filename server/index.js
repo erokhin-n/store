b6e63b0c9718f9f8334a283f2913f34f2be2c806
nikita <erohinnikita100@gmail.com>
2023-09-20 22:56:26 +0530

    front

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const corsOptions = {
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token', 'Authorization'],
     credentials: true,
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: "http://localhost:3000",
+    origin: "https://frontend-s4ut.onrender.com",
     preflightContinue: false,
 };
 
