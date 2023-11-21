af301c750b47c8a2ca0c9098830b9a1defd7c1b0
nikita <erohinnikita100@gmail.com>
2023-11-21 14:23:50 +0530

    show_err

--- server/middleware/errorHandlingMiddleware.js
+++ server/middleware/errorHandlingMiddleware.js
@@ -4,6 +4,6 @@ module.exports = function(err, req, res, next) {
 	if(err instanceof ApiError) {
 		return res.status(err.status).json({message: err.message})
 	}
-
-	return res.status(500).json({message: "непредвиденная ошибка!"})
+	console.log(err.message)
+	return res.status(500).json({message: err.message})
 }
