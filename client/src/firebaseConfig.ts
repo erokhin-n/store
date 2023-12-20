f523225ea2f05efb4dfcd7eebd2a05bd0450d260
nikita <erohinnikita100@gmail.com>
2023-12-20 17:33:33 +0530

    show_filePath4

--- client/src/firebaseConfig.ts
+++ client/src/firebaseConfig.ts
@@ -8,11 +8,17 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBYLLg1BO0hccqu1fLOGqqUjUHwjR2sCZE",
+
   authDomain: "storepictures-db9c6.firebaseapp.com",
+
   projectId: "storepictures-db9c6",
+
   storageBucket: "storepictures-db9c6.appspot.com",
+
   messagingSenderId: "152604144843",
+
   appId: "1:152604144843:web:9a44fdd9327fbb9502174e",
+
   measurementId: "G-XG4T2EPTQY"
 };
 
