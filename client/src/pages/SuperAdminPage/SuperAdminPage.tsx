b4ea19ae4696522c744cede83eef8c44e9d42853
nikita <erohinnikita100@gmail.com>
2023-12-23 15:52:54 +0530

    done

--- client/src/pages/SuperAdminPage/SuperAdminPage.tsx
+++ client/src/pages/SuperAdminPage/SuperAdminPage.tsx
@@ -3,7 +3,6 @@ import { LoginActions } from "../../App"
 import AuthForm from "../../components/AuthForm/AuthForm"
 import UserList from "../../components/UserList/UserList"
 import { formView } from "../../enums/enums"
-import style from './SuperAdminPage.module.css'
 
 const SuperAdminPage = () => {
 
@@ -15,7 +14,7 @@ const SuperAdminPage = () => {
 
 
     return (
-        <section className={style.body} style={{background: 'darkgray', height: 'auto'}}>
+        <section style={{background: 'darkgray', height: 'auto'}}>
             <div>
                 <h3>регистрация администратора</h3>
                 <AuthForm/>   
